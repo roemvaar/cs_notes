c47244564364d6dbe7703598d6738e76b9886c7a
Roberto Valenzuela <valenzuelarober@gmail.com>
2020-11-11 15:15:55 -0600

    5 - stack using linked lists

---
+++ pie/5/stack.c
@@ -0,0 +1,27 @@
+/*
+ * 5. Linked Lists
+ *
+ * Problem: Discuss the stack data structure. Implement a stack in C using either 
+ * a linked list or a dynamic array, and justify your decision. Design the interface 
+ * to your stack to be complete, consistent and easy to use.
+ *
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+struct ListNode {
+    int data;
+    struct ListNode *next;
+};
+
+
+int main() {
+
+    printf("Stack - Using Linked List\n");
+    return (0);
+
+}
+
