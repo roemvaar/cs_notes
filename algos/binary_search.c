5963964e029f9c02d720adb0523c029ad68ecc3b
Roberto Valenzuela <valenzuelarober@gmail.com>
2020-12-16 23:46:22 -0600

    Binary search first draft

---
+++ algos/binary_search.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int binarySearch(int * list, int left, int right, int value)
+{
+    printf("Binary Search in C");
+    
+    return(0);
+}
+
+
+int main(void)
+{
+    int list[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    binarySearch();
+    
+    return 0;
+}
