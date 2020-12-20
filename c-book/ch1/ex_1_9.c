949621bdfaa2c20775e6c36a8adc168371c44c50
Roberto Valenzuela <valenzuelarober@gmail.com>
2020-12-20 16:11:12 -0600

    Exercise 1.9

--- c-book/ch1/ex_1_9.c
+++ c-book/ch1/ex_1_9.c
@@ -1,13 +1,26 @@
-#include <stdio.h>
-
-
 // Exercise 1.9 Write a program to copy its input to its output, replacing each
 // string of one or more blanks by a single blank
 
+
+#include <stdio.h>
+
+
 int main(void)
 {
-    
+    int c;
+    int last_c;
 
+    while((c = getchar()) != EOF)
+    {
+        if((c == ' ') && (last_c == ' '))
+        {
+            continue;
+        }
+
+        putchar(c);
+
+        last_c = c;
+    }
 
     return 0;
 }
