e87414e0f8defeffdf57505b7e11205965dd19f0
Roberto Valenzuela <valenzuelarober@gmail.com>
2020-12-20 15:27:12 -0600

    Algos: merge and selection sort algorithms templates

--- algos/merge_sort.c
+++ algos/merge_sort.c
@@ -1,1 +1,15 @@
 // Merge sort
+//
+// Log-linear sorting algorithm
+
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    printf("Merge sort algorithm implementation in C\n");
+
+    return 0;
+}
+
---
+++ algos/selection_sort.c
@@ -0,0 +1,15 @@
+// Selection sort
+//
+// Quadratic sorting algorithm 
+
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    printf("Selection sort algorithm implementation in C\n");
+
+    return 0;
+}
+
