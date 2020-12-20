a538acdefd3eaa02ec5dfb1d1f4dc9e3c5b94f02
Roberto Valenzuela <valenzuelarober@gmail.com>
2020-12-20 15:22:58 -0600

    DS: Queue and stack templates

--- ds/stack.c
+++ ds/stack.c
@@ -1,1 +1,24 @@
 // Stack
+//
+// Last In First Out
+//
+// Basic operations:
+// push - add an item at the top of the stack
+// pop - retrieve and remove the item on top of the stack
+//
+// "Advanced" operations:
+// empty - check whether the stack is empty
+// number of elements - get the number of items currently in the stack
+
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    printf("Stack implementation in C\n");
+
+
+    return 0;
+}
+
