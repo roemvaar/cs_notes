e6181cabda2da9d5df1bff8b4aac4b93e9a64606
Roberto Valenzuela <valenzuelarober@gmail.com>
2020-12-19 21:06:44 -0600

    Exercise 1.10 and 1.9 templates

---
+++ c-book/ch1/ex_1_10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+// Exercise 1.10 Write a program to copy its input to its output, replacing each
+// tab by \t, each backspace by \b, and each backslash by \\. This makes tabs and
+// and backspaces visible in an unambiguous way.
+
+int main(void)
+{
+    
+
+
+    return 0;
+}
+
---
+++ c-book/ch1/ex_1_9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+// Exercise 1.9 Write a program to copy its input to its output, replacing each
+// string of one or more blanks by a single blank
+
+int main(void)
+{
+    
+
+
+    return 0;
+}
+
