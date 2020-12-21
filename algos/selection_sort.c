d73010c00b0054ad34de8511d70970fad04e17d0
Roberto Valenzuela <valenzuelarober@gmail.com>
2020-12-21 11:26:44 -0600

    Selection sort: first draft

--- algos/selection_sort.c
+++ algos/selection_sort.c
@@ -1,15 +1,81 @@
 // Selection sort
 //
-// Quadratic sorting algorithm 
+// Quadratic sorting algorithm
+//
+// The selection sort algorithm sorts an array by repeatedly finding the 
+// minimum element (considering ascending order) from unsorted part and 
+// putting it at the beginning.
 
 
 #include <stdio.h>
+
+#define SIZE(A) (sizeof(A)/sizeof(A[0]))
+
+void selection_sort(int *array, int size);
+static void swap_elements(int *xp, int *yp);
+void print_array(int * array, int size);
 
 
 int main(void)
 {
     printf("Selection sort algorithm implementation in C\n");
+    
+    int array[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
 
+    // Unsorted array
+    printf("Unsorted array\n"); 
+    print_array(array, SIZE(array));
+    
+    selection_sort(array, SIZE(array));
+
+    // Sorted array
+    printf("Sorted array\n");
+    print_array(array, SIZE(array));
+    
     return 0;
+}
+
+
+void selection_sort(int *array, int size)
+{
+    int i;
+    int j;
+    int smallest;
+    
+    for(i = 0; i < size - 1; i++)
+    {
+        smallest = i;
+
+        for(j = i+1; j < size; j++)
+        {
+            if(array[i] > array[j])
+            {
+                smallest = j;
+            }
+        }
+
+        swap_elements(&array[smallest], &array[i]);
+    }
+}
+
+
+static void swap_elements(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+
+void print_array(int * array, int size)
+{
+    int i;
+
+    for(i = 0; i < size; i++)
+    {
+        printf("%d", array[i]);
+    }
+
+    putchar('\n');
 }
 
