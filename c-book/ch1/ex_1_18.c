ac7daca5d45efbb809066fd4c88ba72342ea6e29
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-02-11 18:04:15 -0600

    Update ex_1_18.c

--- c-book/ch1/ex_1_18.c
+++ c-book/ch1/ex_1_18.c
@@ -1,8 +1,18 @@
+// Ch 1 - Ex 18. Write a program to remove trailing blanks and tabs
+// from each line of input, and to delete entirely blank lines.
+
 #include <stdio.h>
 
 
 int main(void)
 {
+    int c;
+
+    while((c = getchar()) != EOF)
+    {
+        putchar(c);
+    }
+
     return 0;
 }
 
