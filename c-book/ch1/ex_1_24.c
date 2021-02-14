edd4eb7fa192425ae481c2428a9c0ff90bf064ea
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-02-13 23:06:38 -0600

    Create ex_1_24.c

---
+++ c-book/ch1/ex_1_24.c
@@ -0,0 +1,10 @@
+// Exercise 1-24 Write a program to check a C program for rudimentary syntax errors like
+// unbalanced parentheses, brackets and braces. Don't forget about quotesm bot single and
+// double, escape sequences, and comments. (This program is hard if you do it in full generality.)
+
+int main(void)
+{
+
+    return 0;
+}
+
