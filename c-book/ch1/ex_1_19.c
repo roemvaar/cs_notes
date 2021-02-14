bd92ff92702be2e2c0139275bbc5353cc50eb32a
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-02-13 23:21:16 -0600

    Create ex_1_19.c

---
+++ c-book/ch1/ex_1_19.c
@@ -0,0 +1,30 @@
+// Exercise 1-19 Write a function reverse(s) that reverses the character string s.
+// Use it to write a program that reverses its inpute a line at a time.
+
+#include <stdio.h>
+
+
+void reverse(char* s);
+
+
+int main(void)
+{
+    int c;
+    
+    
+    while((c = getchar()) != EOF)
+    {
+    
+    
+    }
+    
+    reverse();
+    
+    return 0;
+}
+
+void reverse(char* s)
+{
+    
+}
+
