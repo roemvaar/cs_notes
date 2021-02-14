05daa47925a2ee36419294159e53ec54904c124c
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-02-13 23:08:07 -0600

    Create ex_1_23.c

---
+++ c-book/ch1/ex_1_23.c
@@ -0,0 +1,9 @@
+// Exercise 1-23 Write a program to remove all comments from a C program. Don't forget
+// to handle quoted strings and character constants properly. C comments do not nets.
+
+int main(void)
+{
+    
+    return 0;
+}
+
