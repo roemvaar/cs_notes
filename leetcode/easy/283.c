29d1f5d4999608d4f876da1790aa0199aa56ed3e
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-02-18 10:21:25 -0600

    Update 283.c

--- leetcode/easy/283.c
+++ leetcode/easy/283.c
@@ -3,4 +3,33 @@
 // maintaining the relative order of the non-zero elements.
 // https://leetcode.com/problems/move-zeroes/
 
-
+void moveZeroes(int* nums, int numsSize)
+{
+    int zeroCount = 0;
+    int currPos = 0;    // Position to store non-zero element
+    
+    // Count zeros
+    for(int i = 0; i < numsSize; i++)
+    {
+        if(nums[i] == 0)
+        {
+            zeroCount++;   
+        }
+    }
+    
+    // Store non-zero elements
+    for(int i = 0; i < numsSize; i++)
+    {
+        if(nums[i] != 0)
+        {
+            nums[currPos] = nums[i];
+            currPos++;
+        }
+    }
+    
+    // Store zero elements at the end of the array
+    for(int i = numsSize - 1; i >= numsSize - zeroCount; i--)
+    {
+        nums[i] = 0;
+    }
+}
