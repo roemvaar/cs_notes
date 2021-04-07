885f06da12a8e5c6077de542c71483335dd2f78d
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-04-06 23:13:33 -0500

    Create ex_2_1_calculate.c

---
+++ books/c-book/ch2/ex_2_1_calculate.c
@@ -0,0 +1,30 @@
+// Exercise 2-1. Write a program to determine the ranges of char, short, int,
+// and long variables, both signed and unsigned, by printing appropriate values
+// from standard headers and by direct computation. Harder if you compute them:
+// determine the ranges of the various floating-point types.
+
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    unsigned char a;
+    signed char b;
+    unsigned short c;
+    signed short d;
+    unsigned int e;
+    signed int f;
+    unsigned long g;
+    signed long h;
+    float i;
+    double j;
+    
+    // Calculate ranges - p.49 https://seriouscomputerist.atariverse.com/media/pdf/book/C%20Answer%20Book.pdf
+    
+  
+  
+    return 0;  
+}
+    
