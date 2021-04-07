37d9bf3e5efb092dfff5c58190f485238d533143
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-04-06 23:12:00 -0500

    ex_2_1.c - print ranges by using standard headers

--- books/c-book/ch2/ex_2_1.c
+++ books/c-book/ch2/ex_2_1.c
@@ -19,6 +19,7 @@ int main(void)
     unsigned long g;
     signed long h;
     float i;
+    double j;
 
     // Printing values from standard headers
     printf("Range of unsigned char: %d - %d\n", 0, UCHAR_MAX);
@@ -29,10 +30,9 @@ int main(void)
     printf("Range of signed int: %d - %d\n", INT_MIN, INT_MAX);
     printf("Range of unsigned long: %d - %ld\n", 0, ULONG_MAX);
     printf("Range of signed long: %ld - %ld\n", LONG_MIN, LONG_MAX);
-    //printf("Range of float %", );
-
-    // Printing values by direct computation
+    printf("Range of float %.5e - %.5e", FLT_MIN, FLT_MAX);
+    printf("Range of double %.5e - %.5e", DBL_MIN, DBL_MAX);
 
     return 0;
 }
-
+ 
