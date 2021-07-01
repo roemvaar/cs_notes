380cb217c170968d9646abab992a033e632acf63
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-07-01 14:29:15 -0500

    stack: implemented using linked lists

--- data_structures/stacks/stacks.h
+++ data_structures/stacks/stacks.h
@@ -8,17 +8,31 @@
  * The push() operation will be passed the data it is to add
  * to the stack, and pop() will return a piece of data from
  * the top of the stack.
+ *
+ * createStack() and deleteStack() functions.
+ *
+ * These functions are used for implementation independence
+ * and consistency. By including this functions, you have the
+ * possibility to change the underlying implementation of the 
+ * stack without needing to change the programs that use the
+ * stack.
+ * 
+ *  General-purpose solution using void pointers.
  */
 
 #ifndef STACKS_H
 #define STACKS_H
+
+#include <stdbool.h>
 
 typedef struct Element {
     struct Element *next;
     void *data;
 } Element;
 
-void push(Element *startOfStack, void *data);
-void *pop(Element *startOfStack);
+void push(Element **stack, void *data);
+void *pop(Element **stack, void **data);
+bool createStack(Element **stack);
+bool deleteStack(Element **stack);
 
 #endif /* STACKS_H */
