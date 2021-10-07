2db01c35342f706c37fe53d092db76b02a4bc5fc
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-10-07 12:53:30 -0500

    Update stack.cpp

--- data_structures/stacks/stack.cpp
+++ data_structures/stacks/stack.cpp
@@ -5,6 +5,8 @@
 // Basic operations:
 // push - add an item at the top of the stack
 // pop - retrieve and remove the item on top of the stack
+// peek - returns the at the top of the stack without removing it 
+// clear - removes all items from the stack
 
 
 #include <iostream>
@@ -12,12 +14,16 @@
 using namespace std;
 
 class Stack {
+    Stack();
+    ~Stack();
     public:
-        push();
+        push(int item);
         pop();
+        peek();
+        clear();
 }
 
-Stack::push() 
+Stack::push(int data) 
 {
 
 }
@@ -27,9 +33,25 @@ Stack::pop()
 
 }
 
+Stack::peek()
+{
+    
+}
+
+Stack::clear()
+{
+    
+}
+
 int main(void)
 {
     std::cout << "Stack implementation in C++ using vectors\n";
+    
+    Stack myStack;
+    
+    for(int i = 1; i <= 10; i++) {
+                
+    }
 
     return 0;
 }
