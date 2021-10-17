cf31e209f23829c4eb9906741dec862e4aa6e83a
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-10-17 13:01:03 -0500

    Update tries.cpp

--- data_structures/trees/tries.cpp
+++ data_structures/trees/tries.cpp
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+class Trie {
+    
+public:
+    bool isLeaf;
+    Trie* character[128];
+    
+    // Constructor
+    Trie()
+    {
+        this->isLeaf = false;
+        
+        for(int i = 0; i < 128; i++) {
+            this->character[i] = nullptr;
+        }
+    }
+    
+    void insert(string);
+    bool deletion(Trie*&, string);
+    bool search(string);
+    bool haveChildren(Trie const*); 
+};
+ 
