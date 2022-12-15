19e526992b8612c5618cfa960c1e95b2c0c3dc10
Roberto Valenzuela <valenzuelarober@gmail.com>
2022-12-15 12:11:58 -0500

    ctci: last k lines circular array solution

--- ctci/12_c++/12_1.cc
+++ ctci/12_c++/12_1.cc
@@ -5,15 +5,11 @@
 #include <fstream>
 #include <string>
 
-// TODO: Improve algorithm by using the ios::end offset counted from the end of the stream
-// Current algorithm
+// Solution 1: Read the file twice
 // 1) Reads all the file and counts the number of lines
 // 2) Returns the stream pointer to the beginning
 // 3) Re-reads all the lines again until it finds the num_lines - k and
 // prints the last k lines
-// This is sub-optimal, we can just start at the end instead, and only read
-// the necessary lines
-
 void LastKLines(int k)
 {
     std::string line;
@@ -50,9 +46,37 @@ void LastKLines(int k)
     }
 }
 
+// Solution 2: Place file pointer at the end and read K lines before the end
+
+// Solution from book - using circular array
+void PrintLast10Lines(std::string filename)
+{
+    const int K = 10;
+    std::ifstream file(filename);
+    std::string L[K];
+    int size = 0;
+
+    // Read file line by line into a circular array
+    // peek() so an EOF following a line ending is not considered a separate line
+    while (file.peek() != EOF) {
+        getline(file, L[size % K]);
+        ++size;
+    }
+
+    // Compute start of circular array, and the size of it
+    int start = size > K ? (size % K) : 0;
+    int count = std::min(K, size);
+
+    // Print elements in the order they were read
+    for (int i = 0; i < count; ++i) {
+        std::cout << L[(start + i) % K] << std::endl;
+    }
+}
+
 int main()
 {
-    LastKLines(10);
+    //LastKLines(10);
+    PrintLast10Lines("lorem_ipsum.txt");
 
     return 0;
 }
