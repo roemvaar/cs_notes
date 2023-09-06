988ad54d41b449b5812e64705209b1f767efc4ed
Roberto Valenzuela <valenzuelarober@gmail.com>
2023-09-05 21:17:55 -0400

    arrays: header file and tester code for scratch implementation

--- c/include/arrays.h
+++ c/include/arrays.h
@@ -1,4 +1,20 @@
 #ifndef ARRAYS_H
 #define ARRAYS_H
 
-#endif /* ARRAYS_H */
+#include <stdbool.h>
+#include <stdio.h>
+
+
+typedef struct {
+    size_t size;
+    size_t element_size;
+    void *elements;
+} _array;
+
+typedef _array array;
+
+array *array_create(size_t size, size_t element_size);
+bool array_set(array *a, size_t index, void *element);
+void *array_get(array *a, size_t index);
+
+#endif // ARRAYS_H
--- c/src/arrays.c
+++ c/src/arrays.c
@@ -1,5 +1,6 @@
-// Arrays in C
+#include "arrays.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 
 #define ARRAY_SIZE 10
@@ -7,20 +8,64 @@
 
 int main(void)
 {
-    int array[ARRAY_SIZE];
-    int i;
+    // int array[ARRAY_SIZE];
+    // int i;
  
-    // Fill array elements
-    for(i = 0; i < ARRAY_SIZE; i++)
-    {
-        array[i] = i;
+    // // Fill array elements
+    // for(i = 0; i < ARRAY_SIZE; i++)
+    // {
+    //     array[i] = i;
+    // }
+ 
+    // // Print array elements
+    // for(i = 0; i < ARRAY_SIZE; i++)
+    // {
+    //     printf("%d\n", array[i]);
+    // }
+
+    array *arr;
+    int size = 10;
+
+    arr = array_create(size, sizeof(int));
+
+    // Fill array
+    for (int i = 0; i < size; i++) {
+        int num = i * i;
+        array_set(arr, i, &num);
     }
- 
+
     // Print array elements
-    for(i = 0; i < ARRAY_SIZE; i++)
-    {
-        printf("%d\n", array[i]);
+    for (int i = 0; i < size; i++) {
+        array_get(arr, i);
     }
 
     return 0;
+}
+
+
+array *array_create(size_t size, size_t element_size)
+{
+    array *a;
+
+    printf("Array created\n");
+
+    return a;
+}
+
+
+bool array_set(array *a, size_t index, void *element)
+{
+    printf("Array set\n");
+
+    return true;
+}
+
+
+void *array_get(array *a, size_t index)
+{
+    void *element;
+
+    printf("Array get\n");
+
+    return element;
 }
